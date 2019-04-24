864dbf43760aad96c132dcfa42002ef7999d3822
koulgar <dogucandurmuslar@gmail.com>
2019-04-24 14:33:48 +0300

    Relocation

--- Projects/cryptocoindemo/src/main/java/com/koulgar/cryptocoindemo/Dao/CryptocoinDao/CryptocoinDao.java
+++
@@ -1,8 +0,0 @@
-package com.koulgar.cryptocoindemo.Dao.CryptocoinDao;
-
-import com.koulgar.cryptocoindemo.Entity.Cryptocoin;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CryptocoinDao extends CrudRepository<Cryptocoin,Integer> {
-
-}
